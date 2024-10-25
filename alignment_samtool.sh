83bdd7ecbd01ac9a76846ef93b9d22ebb990ee66
Salma <salmahafeez575@gmail.com>
2024-10-25 22:34:24 +0500

    Delete alignment_samtool.sh

--- alignment_samtool.sh
+++
@@ -1,8 +0,0 @@
-# Install SAMtools if not already installed
-sudo apt-get install samtools
-
-# Create a directory for BAM results
-mkdir bam_results
-
-# Convert SAM to sorted BAM
-samtools view -Sb hisat2_results/alignment.sam | samtools sort -o bam_results/alignment_sorted.bam
