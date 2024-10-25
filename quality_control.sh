77895c9af0709a019a6b055e735181c5fcd45341
Salma <salmahafeez575@gmail.com>
2024-10-25 22:34:34 +0500

    Delete quality_control.sh

--- quality_control.sh
+++
@@ -1,8 +0,0 @@
-# Install FastQC if not already installed
-sudo apt-get install fastqc
-
-# Create a directory for FastQC results
-mkdir fastqc_results
-
-# Run FastQC on all FASTQ files
-fastqc *.fastq -o fastqc_results/
