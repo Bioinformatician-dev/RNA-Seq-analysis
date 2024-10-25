f16b9895f632119977a904cdbd7b1a5a92ebba1a
Salma <salmahafeez575@gmail.com>
2024-10-25 22:56:55 +0500

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,11 +1,26 @@
-# Install HISAT2 if not already installed
-sudo apt-get install hisat2
+# Check the quality of the raw reads
+fastqc sample_R1.fastq sample_R2.fastq -o fastqc_reports/
 
-# Build HISAT2 index (if not already built)
-hisat2-build genome.fa genome_index
+# Trim the reads to remove low-quality sequences and adapters
+trimmomatic PE -phred33 sample_R1.fastq sample_R2.fastq \
+trimmed_sample_R1_paired.fastq trimmed_sample_R1_unpaired.fastq \
+trimmed_sample_R2_paired.fastq trimmed_sample_R2_unpaired.fastq \
+ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 SLIDINGWINDOW:4:20 MINLEN:36
 
-# Create a directory for alignment results
-mkdir hisat2_results
+# Build the STAR index
+STAR --runMode genomeGenerate --genomeDir ./genome_index \
+--genomeFastaFiles genome.fa --sjdbGTFfile annotations.gtf \
+--sjdbOverhang 99
 
-# Run HISAT2 for alignment
-hisat2 -x genome_index -1 trimmed_reads/output_R1_paired.fastq -2 trimmed_reads/output_R2_paired.fastq -S hisat2_results/alignment.sam
+# Build the STAR index
+STAR --runMode genomeGenerate --genomeDir ./genome_index \
+--genomeFastaFiles genome.fa --sjdbGTFfile annotations.gtf \
+--sjdbOverhang 99
+
+# Align the trimmed reads
+STAR --runThreadN 4 --genomeDir ./genome_index \
+--readFilesIn trimmed_sample_R1_paired.fastq trimmed_sample_R2_paired.fastq \
+--outFileNamePrefix sample_ --outSAMtype BAM SortedByCoordinate
+
+# Count reads that overlap with features in the GTF
+featureCounts -a annotations.gtf -o counts.txt sample_Aligned.sortedByCoord.out.bam
